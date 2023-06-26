a56e9388a74af175057e8457b336e10bd74238ff
sojungpp <sophie2458@naver.com>
2023-06-26 15:09:06 +0900

    project setting

---
+++ src/main/java/com/meongmory/meongmory/MeongmoryApplication.java
@@ -0,0 +1,13 @@
+package com.meongmory.meongmory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongmoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongmoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meongmory/meongmory/MeongmoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meongmory.meongmory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongmoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

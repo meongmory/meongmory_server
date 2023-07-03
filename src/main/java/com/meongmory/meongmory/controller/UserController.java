d1ccc92c1f759779e79aaf4a171382e07cc40106
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2023-07-03 20:01:42 +0900

    #1 feat: test controller 생성

---
+++ src/main/java/com/meongmory/meongmory/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.meongmory.meongmory.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+@RequiredArgsConstructor
+public class UserController {
+
+  @GetMapping("/test")
+  public ResponseEntity<?> testController() {
+    return ResponseEntity.ok("test");
+  }
+}

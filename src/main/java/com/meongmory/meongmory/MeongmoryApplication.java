53f5ec31d533b3035375a5697dcff56ba82f480a
sojungpp <sophie2458@naver.com>
2023-07-03 19:43:46 +0900

    project setting

--- src/main/java/com/meongmory/meongmory/MeongmoryApplication.java
+++ src/main/java/com/meongmory/meongmory/MeongmoryApplication.java
@@ -2,8 +2,9 @@ package com.meongmory.meongmory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MeongmoryApplication {
 
 	public static void main(String[] args) {

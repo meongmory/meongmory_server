1941bbabc481008090012305e3e7ac5033bfcda1
sojungpp <90022940+sojungpp@users.noreply.github.com>
2023-07-08 23:09:44 +0900

    Update MeongmoryApplication.java

--- src/main/java/com/meongmory/meongmory/MeongmoryApplication.java
+++ src/main/java/com/meongmory/meongmory/MeongmoryApplication.java
@@ -3,7 +3,7 @@ package com.meongmory.meongmory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MeongmoryApplication {
 
 	public static void main(String[] args) {

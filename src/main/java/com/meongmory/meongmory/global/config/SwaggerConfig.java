4feeafa283a465cfd2a19fdeb88e747a5fe7cd6b
iseunghag <qw0802@naver.com>
2023-07-19 22:09:18 +0900

    #5 setting:스웨거 config설정 추가

---
+++ src/main/java/com/meongmory/meongmory/global/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.meongmory.meongmory.global.config;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@EnableAutoConfiguration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(this.apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+}

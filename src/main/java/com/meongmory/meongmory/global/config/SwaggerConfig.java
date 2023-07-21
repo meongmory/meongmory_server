90ed77bcc61e391919223174dc3fe6d91e86bc8a
iseunghag <qw0802@naver.com>
2023-07-21 17:28:24 +0900

    5 fix:스웨거버전 변경, SwaggerConfig수정,필요없는 build.gradle주석처리

--- src/main/java/com/meongmory/meongmory/global/config/SwaggerConfig.java
+++ src/main/java/com/meongmory/meongmory/global/config/SwaggerConfig.java
@@ -3,26 +3,44 @@ package com.meongmory.meongmory.global.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
+
+@Configuration
 @EnableSwagger2
-@EnableAutoConfiguration
 public class SwaggerConfig {
+    private static final String API_NAME = "MEONGMORY_SERVER_API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "MEONGMORY_SERVER_API_명세서";
+
 
     @Bean
-    public Docket api() {
+    public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(this.apiInfo())
-                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+
+
 
 
 }

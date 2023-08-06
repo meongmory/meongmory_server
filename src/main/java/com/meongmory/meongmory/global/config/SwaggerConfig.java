a92cf8e5fcf4e02df7894089b345c755bf87427c
iseunghag <qw0802@naver.com>
2023-08-07 01:04:15 +0900

    #33 set:swagger 토큰등록, queryDsl 세팅

--- src/main/java/com/meongmory/meongmory/global/config/SwaggerConfig.java
+++ src/main/java/com/meongmory/meongmory/global/config/SwaggerConfig.java
@@ -14,9 +14,12 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -32,6 +35,7 @@ public class SwaggerConfig {
                 .additionalModels(
                         typeResolver.resolve(BaseRes.class)
                 )
+                .securitySchemes(Arrays.asList(apiKey()))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
@@ -46,6 +50,11 @@ public class SwaggerConfig {
                 .description(API_DESCRIPTION)
                 .build();
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("ACCESS-TOKEN", "ACCESS-TOKEN", "header");
+    }
+
 
 
 

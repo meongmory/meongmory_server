0afeb301f94822764e2a6a76e428e438dc6279ee
sojungpp <sophie2458@naver.com>
2023-08-06 13:15:35 +0900

    #18 fix: CsController 그룹 설정 @Api로 수정

--- src/main/java/com/meongmory/meongmory/domain/cs/controller/CsController.java
+++ src/main/java/com/meongmory/meongmory/domain/cs/controller/CsController.java
@@ -3,18 +3,18 @@ package com.meongmory.meongmory.domain.cs.controller;
 import com.meongmory.meongmory.domain.cs.service.CsService;
 import com.meongmory.meongmory.domain.cs.dto.response.GetNoticesRes;
 import com.meongmory.meongmory.global.response.ResponseCustom;
+import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "cs", description = "CS API")
+@Api(tags = "CS API")
 @RestController
 @RequestMapping(value = "/cs")
 @RequiredArgsConstructor

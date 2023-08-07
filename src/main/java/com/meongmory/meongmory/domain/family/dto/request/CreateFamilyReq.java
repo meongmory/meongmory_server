1efc962d3bdc01204614d5bfc8d2d44c39215c95
chaerlo127 <monynony0203@gmail.com>
2023-08-07 22:02:32 +0900

    #40 feat: 펫 다이어리(가족) 생성 API 스웨거 설정

--- src/main/java/com/meongmory/meongmory/domain/family/dto/request/CreateFamilyReq.java
+++ src/main/java/com/meongmory/meongmory/domain/family/dto/request/CreateFamilyReq.java
@@ -1,5 +1,6 @@
 package com.meongmory.meongmory.domain.family.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 @Data
 @NoArgsConstructor
 public class CreateFamilyReq {
+    @Schema(type = "String", description = "펫 다이어리 이름", example = "댕냥이의 일기장")
     @NotBlank(message = "G0004")
     private String name;
 }

87fbb15cc24d1953c173cb296a723235e39bdaf1
iseunghag <qw0802@naver.com>
2023-08-09 02:43:36 +0900

    #55 add:webconfig추가

---
+++ src/main/java/com/meongmory/meongmory/global/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.meongmory.meongmory.global.config;
+
+import com.meongmory.meongmory.global.resolver.LoginResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginResolver isLoginResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(isLoginResolver);
+    }
+}

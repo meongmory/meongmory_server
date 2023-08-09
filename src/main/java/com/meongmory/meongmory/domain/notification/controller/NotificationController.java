d5ea83adf9a8f59cab6e4641d429650abd4b6a24
sojungpp <sophie2458@naver.com>
2023-08-09 19:06:01 +0900

    #73 fix: authentication 적용

--- src/main/java/com/meongmory/meongmory/domain/notification/controller/NotificationController.java
+++ src/main/java/com/meongmory/meongmory/domain/notification/controller/NotificationController.java
@@ -2,9 +2,12 @@ package com.meongmory.meongmory.domain.notification.controller;
 
 import com.meongmory.meongmory.domain.notification.dto.response.GetNotificationsRes;
 import com.meongmory.meongmory.domain.notification.service.NotificationService;
+import com.meongmory.meongmory.global.resolver.IsLogin;
+import com.meongmory.meongmory.global.resolver.LoginStatus;
 import com.meongmory.meongmory.global.response.ResponseCustom;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -27,8 +30,7 @@ public class NotificationController {
             @ApiResponse(responseCode = "200", description = "알림 목록 조회 성공", content = @Content(schema = @Schema(implementation = GetNotificationsRes.class)))
     })
     @Operation(summary = "알림 목록 조회", description = "알림 전체 목록을 조회합니다.")
-    public ResponseCustom<GetNotificationsRes> getNotifications() {
-        // TODO 유저 토큰 받아오기
-        return ResponseCustom.OK(notificationService.getNotifications(1L));
+    public ResponseCustom<GetNotificationsRes> getNotifications(@Parameter(description = "JWT 토큰 헤더") @IsLogin LoginStatus loginStatus) {
+        return ResponseCustom.OK(notificationService.getNotifications(loginStatus.getUserId()));
     }
 }

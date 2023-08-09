89920feda03051cd588991240dfb4d998ad28325
sojungpp <sophie2458@naver.com>
2023-08-09 19:10:56 +0900

    #73 refactor: 알림 관련 스웨거 설정

--- src/main/java/com/meongmory/meongmory/domain/notification/controller/NotificationController.java
+++ src/main/java/com/meongmory/meongmory/domain/notification/controller/NotificationController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "알림 API")
+@Api(tags = "NOTIFICATION API")
 @RestController
 @RequestMapping(value = "/notification")
 @RequiredArgsConstructor
@@ -27,7 +27,8 @@ public class NotificationController {
 
     @GetMapping("")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "알림 목록 조회 성공", content = @Content(schema = @Schema(implementation = GetNotificationsRes.class)))
+            @ApiResponse(responseCode = "200", description = "(S0001)알림 목록 조회 성공", content = @Content(schema = @Schema(implementation = GetNotificationsRes.class))),
+            @ApiResponse(responseCode = "404", description = "(U0001)존재하지 않는 유저\n", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
     })
     @Operation(summary = "알림 목록 조회", description = "알림 전체 목록을 조회합니다.")
     public ResponseCustom<GetNotificationsRes> getNotifications(@Parameter(description = "JWT 토큰 헤더") @IsLogin LoginStatus loginStatus) {

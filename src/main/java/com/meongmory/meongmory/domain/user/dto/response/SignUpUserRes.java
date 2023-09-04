a00fb5f5d4c05143d27b3ff14529f4bbca6c44aa
iseunghag <qw0802@naver.com>
2023-09-05 00:56:27 +0900

    #94 fix:회원가입 로그인 response수정, tokenutil 리펙토링

--- src/main/java/com/meongmory/meongmory/domain/user/dto/response/SignUpUserRes.java
+++ src/main/java/com/meongmory/meongmory/domain/user/dto/response/SignUpUserRes.java
@@ -8,21 +8,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class SignUpUserRes {
-    private Long userId;
-    private String nickname;
-    private Boolean marketing;
-    private License license;
-    private String imageURL;
     private String accessToken;
 
-    public static SignUpUserRes toDto(User user, String accessToken){
+    public static SignUpUserRes toDto(String token){
         SignUpUserRes signUpUserRes=new SignUpUserRes();
-        signUpUserRes.userId=user.getUserId();
-        signUpUserRes.nickname=user.getNickname();
-        signUpUserRes.marketing=user.getMarketing();
-        signUpUserRes.imageURL=user.getImage();
-        signUpUserRes.license=user.getLicense();
-        signUpUserRes.accessToken=accessToken;
+        signUpUserRes.accessToken=token;
         return signUpUserRes;
     }
 
